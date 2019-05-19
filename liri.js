d4b67d0c9193d4cc5846f784221a6d3b6acbd0d8
Serena Brown <serenagbrown24@gmail.com>
2019-05-18 19:46:31 -0600

    added more code to liri

--- liri.js
+++ liri.js
@@ -1,20 +1,47 @@
-require("dotenv").config();
-
+// require("dotenv").config();
 
 var fs  =  require("fs"); //file system that is aready built.
-var spotify = require('node-spotify-api');
+// var spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var axios = require('axios');
-var request = require("request");
+// var request = require("request");
 
-var spotify = new spotify(keys.spotify);
+// var spotify = new spotify(keys.spotify);
 // user input
-var command = process[2];
-console.log("this is command", + command);
+var command = process.argv[2];
+// console.log("this is command", + command);
 var input = process.argv[3];
-console.log("this is input", + input);
+// console.log("this is input", + input);
+// omdb
+var movieThis = function (input) {
+    var url = "http://www.omdbapi.com/?t=" + input + "&y=&plot=full&tomatoes=true&apikey=trilogy"
+     axios.get(url).then(function(response){
+      console.log("title", response.data.Title);
+      console.log("year", response.data.Year);
+    //   console.log(response.data.imdbRating);
+      console.log("country", response.data.Country);
+      console.log("actor", response.data.Actors);
+      console.log("plot", response.data.Plot);
 
-function liri(command, input) {
+    // console.log("testing");
+});
+}
+
+var concertThis = function(input){
+    console.log("looking for concert");
+    var URL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
+    axios.get(URL).then(function(response){
+        // console.log(response.data)
+        for(var i = 0; i < response.data.length; i++){
+            console.log("vanue" , response.data[i].venue.name)
+            console.log("vanue" , response.data[i].venue.city)
+            console.log("vanue" , response.data[i].venue.region)
+
+
+        }
+    })
+}
+
     switch(command) {
         case "spotify-this":
         console.log("testing");
@@ -22,23 +49,19 @@ function liri(command, input) {
 
     case "movie-this":
     // omdb function
-    omdb(input)
-    console,log("movie testing");
+    movieThis(input)
+    // console,log("movie testing");
     break
 
     case "concert-this":
     // bands in town function
-    console.log("testing concert");
+    concertThis(input)
     break;
 
 
     }
-}
 
-// omdb
-function omdb(input) {
-    var url = "http://www.omdbapi.com"
-}
+
 
 
 // spotify
