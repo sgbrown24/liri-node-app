78e0aff5929a441ecc88572e47c83b8b1be77c7f
Serena Brown <serenagbrown24@gmail.com>
2019-05-20 11:43:26 -0600

    added a readme fire

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # liri-node-app
+
+##LIRI is a command line node app that takes in parameters and gives you back data based off the following parameters:
+
+### concert-this
+
+searches for the Bands in Town Artist Events API and renders the following information about each event to the terminal:
+
+* Name of the venue
+* Venue location
+* Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+
+
+### Spotify-this
+
+searches spotify's API and renders the following information about each event to the terminal:
+
+* Artist(s)
+* The song's name
+* A preview link of the song from Spotify
+* The album that the song is from
+
+### Movie-this
+
+searches the OMDB API and renders the following information about each event to the terminal:
+
+   * Title of the movie.
+   * Year the movie came out.
+   * IMDB Rating of the movie.
+   * Rotten Tomatoes Rating of the movie.
+   * Country where the movie was produced.
+   * Language of the movie.
+   * Plot of the movie.
+   * Actors in the m
+
+
+##Technologies Used
+* Node.js
+* Javascript
+* Request (NPM Package)
+* Node-Spotify-API (NPM Package)
+
+
+
+
+
--- liri.js
+++ liri.js
@@ -1,24 +1,25 @@
 // require("dotenv").config();
 
 var fs  =  require("fs"); //file system that is aready built.
-// var spotify = require('node-spotify-api');
+ var spotify = require('node-spotify-api');
+ var spotify = new spotify(keys.spotify);
 var keys = require("./keys.js");
 var axios = require('axios');
 // var request = require("request");
+var moment = require("moment");
 
-// var spotify = new spotify(keys.spotify);
 // user input
 var command = process.argv[2];
-// console.log("this is command", + command);
 var input = process.argv[3];
-// console.log("this is input", + input);
+
+
 // omdb
 var movieThis = function (input) {
     var url = "http://www.omdbapi.com/?t=" + input + "&y=&plot=full&tomatoes=true&apikey=trilogy"
      axios.get(url).then(function(response){
       console.log("title", response.data.Title);
       console.log("year", response.data.Year);
-    //   console.log(response.data.imdbRating);
+      console.log("rating", response.data.imdbRating);
       console.log("country", response.data.Country);
       console.log("actor", response.data.Actors);
       console.log("plot", response.data.Plot);
@@ -27,14 +28,15 @@ var movieThis = function (input) {
 });
 }
 
+// concert function
 var concertThis = function(input){
     console.log("looking for concert");
     var URL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
     axios.get(URL).then(function(response){
         // console.log(response.data)
         for(var i = 0; i < response.data.length; i++){
-            console.log("vanue" , response.data[i].venue.name)
-            console.log("vanue" , response.data[i].venue.city)
+            console.log("Name of vanue" , response.data[i].venue.name)
+            console.log("vanue location" , response.data[i].venue.city)
             console.log("vanue" , response.data[i].venue.region)
 
 
@@ -42,36 +44,32 @@ var concertThis = function(input){
     })
 }
 
-    switch(command) {
-        case "spotify-this":
-        console.log("testing");
-        break;
-
-    case "movie-this":
-    // omdb function
-    movieThis(input)
-    // console,log("movie testing");
-    break
-
-    case "concert-this":
-    // bands in town function
-    concertThis(input)
-    break;
-
-
-    }
-
-
-
-
 // spotify
-function spotify(input){
-    var url = ""
+var spotifyThis = function(input) {
+   
 }
 
 
 
 
+// switch commands
+    switch(command) {
+        case "spotify-this":
+      spotifyThis(input)
+        break;
+
+    case "movie-this":
+    movieThis(input)
+    break
+
+    case "concert-this":
+    concertThis(input)
+    break;
+
+    case "do-what-it-says":
+    read();
+    break;
+    }
 
 
 
@@ -100,16 +98,11 @@ function spotify(input){
 
 
 
-// //wiriting object to a file
-// function wFile(wFile) {
-//     fs.writeFile('log.json', JSON.stringify(wFile), function(err) {
-//         if (err) {
-//             console.log(err);
-//         }
-//     })
-
-// }
 
 
-// var liri
+
+
+
+
+
 
